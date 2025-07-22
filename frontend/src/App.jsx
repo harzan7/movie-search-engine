a005203fe99927a217645a87ad76dcc281379946
Harza <harzan12345@gmail.com>
2025-07-21 22:58:23 -0400

    Create basic movie card layout.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
+import MovieCard from "./components/MovieCard"
 
 function App() {
   return (
       <>
+          <MovieCard movie={{title: "Tim's Film", release_date: "2024"}}></MovieCard>
       </>
   )
 }
