2b457dad38d6f021b598c62994a1a1ef0ff09803
Harza <harzan12345@gmail.com>
2025-07-22 23:23:20 -0400

    Create basic home page layout with non-working Search bar, and couple movies.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import MovieCard from "./components/MovieCard"
+import Home from "./pages/Home"
 
 function App() {
   return (
       <>
-          <MovieCard movie={{title: "Tim's Film", release_date: "2024"}}></MovieCard>
+          <Home></Home>
       </>
   )
 }
