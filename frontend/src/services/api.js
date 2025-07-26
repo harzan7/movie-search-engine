f889ea0f68b6c7e121f429bee355fc119f07137c
Harza <harzan12345@gmail.com>
2025-07-25 22:45:28 -0400

    Add function for getting popular movies from API.

---
+++ frontend/src/services/api.js
@@ -0,0 +1,8 @@
+const API_KEY = "15eb26c60aebb14e565f4bcfde369275"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await response.json()
+    return data.results
+};
