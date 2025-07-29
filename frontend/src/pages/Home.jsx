8d057d68efd98e22d120b595cc688f7c560fb0bd
Harza <harzan12345@gmail.com>
2025-07-28 23:44:28 -0400

    Add search functionality of movies fetched from API.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -25,11 +25,23 @@ function Home() {
         loadPopularMovies();
     }, [])
 
-    const handleSearch = (e) => {
+    const handleSearch = async (e) => {
         e.preventDefault();
-        alert(searchQuery)
-        setSearchQuery("")
-    }
+        if (!searchQuery.trim()) return
+        if (loading) return
+
+        setLoading(true)
+        try {
+            const searchResults = await searchMovies(searchQuery);
+            setMovies(searchResults);
+            setError(null)
+        } catch (err) {
+            console.log(err)
+            setError("Failed to search movies...")
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <div className="home">
@@ -38,6 +50,8 @@ function Home() {
                     type="text"
                     placeholder="Search for movies..."
                     className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
                 />
                 <button type="submit" className="search-button">Search</button>
             </form>
