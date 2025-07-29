09dffbcd7eef19f2047da425ce8c38295392c96f
Harza <harzan12345@gmail.com>
2025-07-28 23:25:30 -0400

    Add functionality to fetch and load movie info from API.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,35 @@
 import MovieCard from "../components/MovieCard";
+import { useState, useEffect } from "react";
+import { searchMovies, getPopularMovies } from "../services/api.js";
 import "../css/Home.css"
 
 function Home() {
-    const movies = [
-        { id: 1, title: "John Wick", release_date: "2020" },
-        { id: 2, title: "Terminator", release_date: "1999" },
-        { id: 3, title: "The Matrix", release_date: "1998" },
-    ];
+    const [searchQuery, setSearchQuery] = useState("");
+    const [movies, setMovies] = useState([])
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(true);
 
-    const handleSearch = () => {};
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies();
+                setMovies(popularMovies);
+            } catch (err) {
+                console.log(err)
+                setError("Failed to load movies...")
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        loadPopularMovies();
+    }, [])
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(searchQuery)
+        setSearchQuery("")
+    }
 
     return (
         <div className="home">
